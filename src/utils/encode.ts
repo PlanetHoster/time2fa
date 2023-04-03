324c65a6a8a4c41c7d0e355bab122e3f6f41c1f9
Marc-Andre G <magodin.it@gmail.com>
2023-04-03 13:09:58 -0400

    add decode 32 function

--- src/utils/encode.ts
+++ src/utils/encode.ts
@@ -27,3 +27,44 @@ export const Encode32 = (key: Buffer): string => {
   }
   return encodedKey;
 };
+
+export const Decode32 = (s: string): Buffer => {
+  const lookupTable = new Uint8Array(256);
+
+  for (let i = 0; i < BASE32_CHARS.length; i++) {
+    lookupTable[BASE32_CHARS.charCodeAt(i)] = i;
+  }
+
+  let bits = 0;
+  let bitsCount = 0;
+  let result = Buffer.alloc(Math.ceil((s.length * 5) / 8));
+
+  for (let i = 0; i < s.length; i++) {
+    const c = s.charCodeAt(i);
+    if (c === 61) {
+      while (bitsCount >= 8) {
+        bitsCount -= 8;
+        result.writeUInt8((bits >> bitsCount) & 255, (i * 5) >> 3);
+      }
+      break;
+    }
+
+    const value = lookupTable[c];
+
+    if (value === undefined) {
+      throw new Error("Invalid base32 string");
+    }
+
+    bits = (bits << 5) | value;
+    bitsCount += 5;
+    if (bitsCount >= 8) {
+      bitsCount -= 8;
+      result.writeUInt8((bits >> bitsCount) & 255, (i * 5) >> 3);
+    }
+  }
+
+  return result.subarray(
+    0,
+    result.indexOf(0) !== -1 ? result.indexOf(0) : undefined
+  );
+};
